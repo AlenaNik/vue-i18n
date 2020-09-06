1bb6cd1350e1cbe0f5a6faf1859e5c98c6c33d18
Alena <alena.nik.digital@gmail.com>
2020-09-06 14:19:04 +0200

    set locale on button click

--- src/components/Helloi18n.vue
+++ src/components/Helloi18n.vue
@@ -1,12 +1,20 @@
 <template>
     <div>
-        <p>{{ $t("message")}}</p>
         <p>{{ $t("message.greeting")}}</p>
+        <p>{{ $t("message.goodbye")}}</p>
+        <button @click="setLocale('en')">English</button>
+        <button @click="setLocale('es')">Spanish</button>
+        <button @click="setLocale('de')">German</button>
     </div>
 </template>
 
 <script>
 export default {
-  name: "HelloI18n"
+  name: "HelloI18n",
+  methods:{
+    setLocale(locale) {
+      this.$i18n.locale = locale;
+    }
+  }
 }
 </script>
