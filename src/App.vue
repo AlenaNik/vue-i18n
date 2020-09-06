9153855b19a847481d665950d282b58aa02528e8
Alena <alena.nik.digital@gmail.com>
2020-09-06 17:01:16 +0200

    local storage and browser

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,15 @@ export default {
   name: 'App',
   components: {
     HelloI18n
-  }
+  },
+    created() {
+      const locale = localStorage.getItem('locale')
+      if (locale) {
+          this.$i18n.locale = locale;
+      } else if (navigator.language) {
+          this.$i18n.locale = navigator.language.substring(0, 2);
+      }
+    }
 }
 </script>
 
--- src/components/Helloi18n.vue
+++ src/components/Helloi18n.vue
@@ -14,7 +14,17 @@ export default {
   methods:{
     setLocale(locale) {
       this.$i18n.locale = locale;
+      localStorage.setItem("locale", locale);
     }
   }
 }
 </script>
+<style>
+    button {
+        margin: 10px;
+        padding: 10px 20px;
+        border: 2px solid plum;
+        border-radius: 8px;
+        background-color: white;
+    }
+</style>
